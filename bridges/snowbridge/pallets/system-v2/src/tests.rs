72d1432fae4e35346a73736e7ac5a7c238590c7d
ron <yrong1997@gmail.com>
2025-02-20 23:10:28 +0800

    Fix runtime api & more tests

--- bridges/snowbridge/pallets/system-v2/src/tests.rs
+++ bridges/snowbridge/pallets/system-v2/src/tests.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
 use crate::{mock::*, *};
-use frame_support::{assert_noop, assert_ok};
+use frame_support::assert_ok;
+use sp_keyring::sr25519::Keyring;
+use xcm::{latest::WESTEND_GENESIS_HASH, prelude::*};
 
 #[test]
 fn register_tokens_succeeds() {
@@ -15,5 +17,27 @@ fn register_tokens_succeeds() {
 			Default::default(),
 			1
 		));
+	});
+}
+
+#[test]
+fn agent_id_from_location() {
+	new_test_ext(true).execute_with(|| {
+		let bob: AccountId = Keyring::Bob.into();
+		let origin = Location::new(
+			1,
+			[
+				Parachain(1000),
+				AccountId32 {
+					network: Some(NetworkId::ByGenesis(WESTEND_GENESIS_HASH)),
+					id: bob.into(),
+				},
+			],
+		);
+		let agent_id = EthereumSystemV2::location_to_message_origin(&origin).unwrap();
+		let expected_agent_id =
+			hex_literal::hex!("6be45a5822eca69502b722ee5130497d63f0358edba9f0dde28e94e152a74742")
+				.into();
+		assert_eq!(agent_id, expected_agent_id);
 	});
 }
