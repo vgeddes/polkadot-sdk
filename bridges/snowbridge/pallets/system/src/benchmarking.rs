0a5032b6b881bc2ec69fb4d8097267b81b5f4cc7
ron <yrong1997@gmail.com>
2025-03-10 18:10:40 +0800

    Fix test

--- bridges/snowbridge/pallets/system/src/benchmarking.rs
+++ bridges/snowbridge/pallets/system/src/benchmarking.rs
@@ -12,14 +12,6 @@ use snowbridge_outbound_queue_primitives::OperatingMode;
 use sp_runtime::SaturatedConversion;
 use xcm::prelude::*;
 
-#[allow(clippy::result_large_err)]
-fn fund_sovereign_account<T: Config>(para_id: ParaId) -> Result<(), BenchmarkError> {
-	let amount: BalanceOf<T> = (10_000_000_000_000_u64).saturated_into::<u128>().saturated_into();
-	let sovereign_account = sibling_sovereign_account::<T>(para_id);
-	T::Token::mint_into(&sovereign_account, amount)?;
-	Ok(())
-}
-
 #[benchmarks]
 mod benchmarks {
 	use super::*;
