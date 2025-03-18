b65c250a3f70e03b41f3c4b9479c18487ecca3e5
ron <yrong1997@gmail.com>
2025-03-18 11:28:34 +0800

    XcmExportFeeToSibling to V1 and AssetOwner to V2

--- bridges/snowbridge/runtime/runtime-common/src/lib.rs
+++ bridges/snowbridge/runtime/runtime-common/src/lib.rs
@@ -4,13 +4,9 @@
 //!
 //! Common traits and types shared by runtimes.
 #![cfg_attr(not(feature = "std"), no_std)]
-
-pub mod fee_handler;
-pub mod register_token;
-
-pub use fee_handler::XcmExportFeeToSibling;
-
-pub use register_token::{ForeignAssetOwner, LocalAssetOwner};
-
 #[cfg(test)]
 mod tests;
+pub mod v1;
+pub mod v2;
+pub use v1::fee_handler::XcmExportFeeToSibling;
+pub use v2::register_token::{ForeignAssetOwner, LocalAssetOwner};
